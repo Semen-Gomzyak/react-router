036f170807a20db8be216cdda72d2aac69c5230f
Semen-Gomzyak <Blacklife1817@gmail.com>
2022-10-31 16:59:40 +0100

    react-router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,32 @@
+import { lazy } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { SharedLayout } from './SharedLayout/SharedLayout';
+
+
+const Home = lazy(() => import('./pages/Home').then(module => ({...module, default: module.Home})));
+const About = lazy(() => import('./pages/About').then(module => ({...module, default: module.About})));
+const NotFound = lazy(() => import('./pages/NotFound').then(module => ({ ...module, default: module.NotFound })));
+const ProductDetails = lazy(() => import('./pages/ProductDetails').then(module => ({ ...module, default: module.ProductDetails })));
+const Products = lazy(() => import('./pages/Products').then(module => ({ ...module, default: module.Products })));
+const Mission = lazy(() => import('./AboutPage/Mission').then(module => ({ ...module, default: module.Mission })));
+const Team = lazy(() => import('./AboutPage/Team').then(module => ({ ...module, default: module.Team })));
+const Reviews = lazy(() => import('./AboutPage/Reviews').then(module => ({...module, default: module.Reviews})));
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Home />} />
+        <Route path="/about" element={<About />}>
+          <Route path="mission" element={<Mission />} />
+          <Route path="team" element={<Team />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
+        <Route path="/products" element={<Products />} />
+        <Route path="products/:id" element={<ProductDetails />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   );
 };
---
+++ src/components/pages/Products.jsx
@@ -0,0 +1,26 @@
+import { useSearchParams } from 'react-router-dom';
+import { getProducts } from 'fakeApi';
+import { ProductList } from '../ProductList/ProductList';
+import { SearchBox } from 'components/SearchBox/SearchBox';
+
+export const Products = () => {
+  const products = getProducts();
+  const [searchParams, setSearchParams] = useSearchParams();
+  const productName = searchParams.get('name') ?? '';
+
+  const visibleProducts = products.filter(product =>
+    product.name.toLowerCase().includes(productName.toLowerCase())
+  );
+
+  const updateQueryString = name => {
+    const nextParams = name !== '' ? { name } : {};
+    setSearchParams(nextParams);
+  };
+
+  return (
+    <main>
+      <SearchBox value={productName} onChange={updateQueryString} />
+      <ProductList products={visibleProducts} />
+    </main>
+  );
+};
